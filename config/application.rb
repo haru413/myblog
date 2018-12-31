1fa1e88b3edac0d029730ce9ed8beb3dd7d29e95
Haru <newhorizon413@gmail.com>
2018-12-31 11:08:22 +0000

    rails_admin setting finished

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Myblog
     
     #Add
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -20,7 +21,6 @@ module Myblog
     # the framework and any gems in your application.
     
 
-    
     #Add
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
