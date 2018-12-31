d7f57bdc4949458da9b1c7cb69ba9d697e61cd1d
Haru <newhorizon413@gmail.com>
2018-12-31 03:20:21 +0000

    finished setting rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_150005) do
+ActiveRecord::Schema.define(version: 2018_12_30_072429) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2018_12_28_150005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
